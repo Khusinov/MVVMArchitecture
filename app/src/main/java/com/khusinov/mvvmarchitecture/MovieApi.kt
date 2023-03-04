2aae02908c13ed353d7472a76510dd941f204f34
Shaxriyor <shakhriyordev@gmail.com>
2023-03-04 16:18:25 +0500

    Initial commit

---
+++ app/src/main/java/com/khusinov/mvvmarchitecture/MovieApi.kt
@@ -0,0 +1,10 @@
+package com.khusinov.mvvmarchitecture
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MovieApi {
+    @GET("popular?")
+    fun getPopularMovies(@Query("api_key") api_key: String): Call<Movies>
+}
